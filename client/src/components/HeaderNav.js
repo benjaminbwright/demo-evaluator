62461657a1e26cbe2adf09def100a6aa4a7687d6
Benjamin Wright <bwright@trilogyed.com>
2019-08-09 19:36:57 -0400

    Added background color and width to header nav

--- client/src/components/HeaderNav.js
+++ client/src/components/HeaderNav.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './HeaderNav.scss';
 
 
 export default function HeaderNav() {
   return(
-    <nav style={styles.nav}>
-      <li style={styles.li}>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/projects">Projects</Link>
-      </li>
+    <nav id="HeaderNav">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+      </ul>
     </nav>
   );
-}
-
-const styles = {
-  nav: {
-    "background":"red",
-  },
-  li: {
-    "color":"white"
-  }
 }
