f65bf2f446513e9eaf4631fd281e3f69c6df8b7a
Benjamin Wright <bwright@trilogyed.com>
2019-08-09 11:42:50 -0400

    Added dotenv and made port dynamic

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 var express = require('express');
 var app = express();
 
-// Set the port (need to add environment variable)
-var PORT = 5000;
+require('dotenv').config()
 
+// Set the port (need to add environment variable)
+var PORT = process.env.PORT;
+
+// Put this in s routes folder
 app.get("/",(req, res) => {
   res.send("The home page.");
 });
