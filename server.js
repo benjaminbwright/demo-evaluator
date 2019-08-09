03f794446bc5309be7a9fec66c1a4efb910441a2
Benjamin Wright <bwright@trilogyed.com>
2019-08-09 11:39:52 -0400

    Added a home route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var app = express();
 // Set the port (need to add environment variable)
 var PORT = 5000;
 
+app.get("/",(req, res) => {
+  res.send("The home page.");
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
